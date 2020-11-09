802acaebe1f457014ad1e897189a0dc3f1fd873d
brandonnly <bly852@gmail.com>
2020-11-09 18:05:39 -0500

    modified stage names into settings.py

--- bot.py
+++ bot.py
@@ -7,6 +7,27 @@ from discord.ext import commands
 bot = discord.ext.commands.Bot('ve!')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
+# get stage list from settings file
+starter_stages = ""  # blank string
+# loop through starter stages
+for x in range(len(settings.starter_stages)):
+    # if first stage then make string the stage name
+    if len(starter_stages) == 0:
+        starter_stages = f"{settings.starter_stages[x]}\n"
+    # else concatenate the new stage name
+    else:
+        starter_stages = f"{starter_stages}{settings.starter_stages[x]}\n"
+
+counterpick_stages = ""  # blank string
+# loop through counterpick stages
+for x in range(len(settings.counterpick_stages)):
+    # if first stage then make string the stage name
+    if len(counterpick_stages) == 0:
+        counterpick_stages = f"{settings.counterpick_stages[x]}\n"
+    # else concatenate the new stage name
+    else:
+        counterpick_stages = f"{counterpick_stages}{settings.counterpick_stages[x]}\n"
+
 
 @bot.command()
 async def veto(ctx, game, p2):
@@ -21,29 +42,20 @@ async def veto(ctx, game, p2):
         # game 1 embed line
         embed.add_field(name="`Game 1                                               `",
                         value="**Winner:** TBD", inline=False)
-        embed.add_field(name="Starter Stages",
-                        value="Small Battlefield\nPokemon Stadium 2\nSmashville\nTown and City\nFinal Destination",
-                        inline=True)
-        embed.add_field(name="Counterpick Stages", value="Kalos Pokemon League\nLylat Cruise\nYoshi's Story",
-                        inline=True)
+        embed.add_field(name="Starter Stages", value=starter_stages, inline=True)
+        embed.add_field(name="Counterpick Stages", value=counterpick_stages, inline=True)
 
         # game 2 embed line
         embed.add_field(name="`Game 2                                               `",
                         value="**Winner:** TBD", inline=False)
-        embed.add_field(name="Starter Stages",
-                        value="Small Battlefield\nPokemon Stadium 2\nSmashville\nTown and City\nFinal Destination",
-                        inline=True)
-        embed.add_field(name="Counterpick Stages", value="Kalos Pokemon League\nLylat Cruise\nYoshi's Story",
-                        inline=True)
+        embed.add_field(name="Starter Stages", value=starter_stages, inline=True)
+        embed.add_field(name="Counterpick Stages", value=counterpick_stages, inline=True)
 
         # game 3 embed line
         embed.add_field(name="`Game 3                                               `",
                         value="**Winner:** TBD", inline=False)
-        embed.add_field(name="Starter Stages",
-                        value="Small Battlefield\nPokemon Stadium 2\nSmashville\nTown and City\nFinal Destination",
-                        inline=True)
-        embed.add_field(name="Counterpick Stages", value="Kalos Pokemon League\nLylat Cruise\nYoshi's Story",
-                        inline=True)
+        embed.add_field(name="Starter Stages", value=starter_stages, inline=True)
+        embed.add_field(name="Counterpick Stages", value=counterpick_stages, inline=True)
         embed.set_footer(icon_url="https://vaughanesports.org/assets/Vaughan%20Esports%20Logo.png",
                          text=settings.tourney_name)
         await ctx.send(embed=embed)
--- settings.py
+++ settings.py
@@ -1,2 +1,5 @@
 tourney_name = "November 2020 Smash Ult. Monthly"
 
+# smash stages
+starter_stages = ["Small Battlefield", "Pokemon Stadium 2", "Smashville", "Town and City", "Final Destination"]
+counterpick_stages = ["Kalos Pokemon League", "Lylat Cruise", "Yoshi's Story"]
