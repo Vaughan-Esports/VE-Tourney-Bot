7f8ecee74c93c5db6739e7ab4ebdef4a4a8efbb4
brandonnly <bly852@gmail.com>
2020-11-12 21:35:11 -0500

    added newline var

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ tourney_name = "November 2020 Smash Ult. Monthly"
 footer_note = "Ping Brandon for help."
 footer_icon = "https://vaughanesports.org/assets/Vaughan%20Esports%20Logo.png"  # must be URL
 rulebook_url = "https://vaughanesports.org/rules"  # must be URL
+newline = "_ _\n"
 
 # smash stages
 starter_stages = ["Small Battlefield", "Pokemon Stadium 2", "Smashville", "Town And City", "Final Destination"]
