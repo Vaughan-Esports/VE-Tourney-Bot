3bf47c26b0a655e514723faf74d3ed08d0279983
brandonnly <brandon@brandonly.me>
2021-02-20 20:06:31 -0500

    feat: string representation of map based on state

--- base/map.py
+++ base/map.py
@@ -4,3 +4,17 @@ from abc import ABC
 class Map(ABC):
     def __init__(self, name: str):
         self.name = name
+        self.veto = False
+        self.chosen = False
+
+    def __str__(self) -> str:
+        """
+        String representation of a map
+        :return:
+        """
+        if self.veto:
+            return f'~~{self.name}~~'
+        elif self.chosen:
+            return f'⮕**{self.name}~~'
+        else:
+            return self.name
