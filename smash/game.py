2c6b0976b99b90ccc756140ccc0aebc068d9c86d
brandonnly <brandon@brandonly.me>
2021-02-21 03:12:06 -0500

    refactor: move stage veto back to stagelist

--- smash/game.py
+++ smash/game.py
@@ -1,5 +1,3 @@
-from string import capwords
-
 from base.game import Game
 from settings import stages
 from smash.stagelist import StageList
@@ -13,6 +11,8 @@ class Game(Game):
     def __init__(self, game_num: int):
         super().__init__(game_num)
         self.stagelist: StageList = StageList(stages)
+        # stage veto / selection process state
+        self.state = 0
 
     def starters_embed(self) -> str:
         """
@@ -33,30 +33,3 @@ class Game(Game):
         for x in range(len(self.stagelist.counters)):
             message = f'{message}{self.stagelist.counters[x]}\n'
         return message
-
-    def veto(self, stage: str) -> bool:
-        """
-        Tries to veto a stage
-        :param stage: stage to veto
-        :return: boolean value if stage was veto'd
-        """
-        # search for stage in starters
-        for x in range(len(self.stagelist.starters)):
-            # check for name or alias match
-            if capwords(stage) == self.stagelist.starters[x].name \
-                    or stage.lower in self.stagelist.starters[x].aliases:
-                # if matching, change the state of the stage
-                self.stagelist.starters[x].veto = True
-                return True
-
-        # search through counters
-        for x in range(len(self.stagelist.counters)):
-            # check for name or alias match
-            if capwords(stage) == self.stagelist.counters[x].name \
-                    or stage.lower in self.stagelist.starters[x].aliases:
-                # if matching, change the state of the stage
-                self.stagelist.counters[x].veto = True
-                return True
-
-        # returns false if it wasn't successful
-        return False
--- smash/stagelist.py
+++ smash/stagelist.py
@@ -1,3 +1,4 @@
+from string import capwords
 from typing import List
 
 from smash.stage import Stage
@@ -21,3 +22,30 @@ class StageList:
                 self.counters.append(Stage(stages[x]['name'],
                                            stages[x]['starter',
                                                      stages[x]['aliases']]))
+
+    def veto(self, stage: str) -> bool:
+        """
+        Tries to veto a stage
+        :param stage: stage to veto
+        :return: boolean value if stage was veto'd
+        """
+        # search for stage in starters
+        for x in range(len(self.starters)):
+            # check for name or alias match
+            if capwords(stage) == self.starters[x].name \
+                    or stage.lower in self.starters[x].aliases:
+                # if matching, change the state of the stage
+                self.starters[x].veto = True
+                return True
+
+        # search through counters
+        for x in range(len(self.counters)):
+            # check for name or alias match
+            if capwords(stage) == self.counters[x].name \
+                    or stage.lower in self.starters[x].aliases:
+                # if matching, change the state of the stage
+                self.counters[x].veto = True
+                return True
+
+        # returns false if it wasn't successful
+        return False
