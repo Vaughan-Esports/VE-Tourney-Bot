106a353909a3ce8f575b75b64329425300252e36
brandonnly <brandon@brandonly.me>
2021-02-20 20:17:48 -0500

    create: smash stage class

--- smash/stage.py
+++ smash/stage.py
@@ -1,7 +1,20 @@
-class Stage:
-    name = None
-    removed = None
+from typing import List
 
-    def __init__(self, name: str, removed: bool = False):
-        self.name = name
-        self.removed = removed
+from base.map import Map
+
+
+class Stage(Map):
+    """
+    Represents a Smash stage
+    """
+
+    def __init__(self, name: str, starter: bool, aliases: List[str] = None):
+        """
+        Constructor Method
+        :param name: name of the stage
+        :param starter: if stage is a starter
+        :param aliases: alias names for the stage
+        """
+        super().__init__(name)
+        self.starter = starter
+        self.aliases = aliases
