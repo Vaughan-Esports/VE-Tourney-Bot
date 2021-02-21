5dd3197e4942e2c69d4a93e3d672569fc9bce898
brandonnly <brandon@brandonly.me>
2021-02-21 02:47:30 -0500

    feat: veto method for stagelist

--- smash/stagelist.py
+++ smash/stagelist.py
@@ -1,3 +1,4 @@
+from string import capwords
 from typing import List
 
 from smash.stage import Stage
@@ -41,3 +42,30 @@ class StageList:
         for x in range(len(self.counters)):
             message = f'{message}{self.counters[x]}\n'
         return message
+
+    def veto(self, stage: str) -> bool:
+        """
+        Tries to veto a stage
+        :param stage: stage to veto
+        :return: boolean value if stage was veto'd
+        """
+        # search for stage in starters
+        for x in range(len(self.starters)):
+            # check for name or alias match
+            if capwords(stage) == self.starters[x].name \
+                    or stage.lower in self.starters[x].aliases:
+                # if matching, change the state of the stage
+                self.starters[x].veto = True
+                return True
+
+        # search through counters
+        for x in range(len(self.counters)):
+            # check for name or alias match
+            if capwords(stage) == self.counters[x].name \
+                    or stage.lower in self.starters[x].aliases:
+                # if matching, change the state of the stage
+                self.counters[x].veto = True
+                return True
+
+        # returns false if it wasn't successful
+        return False
