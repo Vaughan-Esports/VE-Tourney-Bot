43f3fc3c6cedfcfa2ea95702677c9c013c2d4093
brandonnly <brandon@brandonly.me>
2021-02-20 21:28:43 -0500

    docs: docstring for game constructor

--- base/game.py
+++ base/game.py
@@ -11,11 +11,16 @@ class Game(ABC):
     Base game class, represents a game object
     """
 
-    def __init__(self, match_num: int, map_pool: List[Map]):
-        self.match_num = match_num
+    def __init__(self, game_num: int, map_pool: List[Map]):
+        """
+        Constructor method for a game
+        :param game_num: game number (starts at 0)
+        :param map_pool: List of maps for the game
+        """
+        self.match_num = game_num
         self.map_pool = map_pool
 
-        self.name = f'Game {match_num}'
+        self.name = f'Game {game_num + 1}'
 
         self.selected_map: Map
         self.winner: discord.Member
